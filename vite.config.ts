2f63883ee3d4eb0e95b3ad031dafa2d885f6eb03
gooseDes <sasaprst32@gmail.com>
2026-06-14 00:07:16 +0000

    fix: gh pages should now work

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
             declarationMap: true,
         }),
     ],
+    base: "./",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
