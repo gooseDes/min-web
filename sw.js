33698f11211709b8bf56f88ed554437cba471733
gooseDes <sasaprst32@gmail.com>
2026-07-03 01:58:35 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-// Minimal service worker to satisfy PWA install requirements
+const apiUrl = new URL(self.location).searchParams.get("apiUrl");
+let currentAppState = "normal";
+
 self.addEventListener("install", event => {
     self.skipWaiting();
 });
@@ -9,4 +11,29 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(fetch(event.request));
+});
+
+self.addEventListener("message", event => {
+    if (event.data && event.data.type === "SET_APP_STATE") {
+        currentAppState = event.data.state;
+    }
+});
+
+self.addEventListener("push", event => {
+    const data = event.data.json();
+
+    console.log("push", data);
+
+    const author = data.author;
+
+    if (currentAppState === `chat-${data.chat.id}`) return;
+
+    event.waitUntil(
+        self.registration.showNotification(author.username, {
+            body: data.message,
+            icon: `${apiUrl}/avatars/${author.avatar}.webp`,
+            badge: `/badge.png`,
+            tag: data.chat.id,
+        }),
+    );
 });
